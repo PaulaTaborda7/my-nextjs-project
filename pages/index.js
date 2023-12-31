fa923e7f2ee31f4d601281b54ba15e3321094538
PaulaTaborda7 <ptabordam@unal.edu.co>
2023-12-31 17:07:44 -0500

    Dynamic generation of the resquests to the API

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,7 @@ export default function Home( {articles} ) { // The prop 'articles' give the new
   )
 }
 
+// N - request --> N times executed - Useful for light data or a lot of dynamic data
 // Retrieve th articles in the server, when the user go to the page, it has the articles already
 export async function getServerSideProps() {
   const response = await fetch('https://newsapi.org/v2/everything?q=apple&from=2023-12-30&to=2023-12-30&sortBy=popularity&apiKey=54b9b87b6e6a40cba3c434cdc8aee8a5'
